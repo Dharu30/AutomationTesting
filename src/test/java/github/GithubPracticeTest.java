f5598ac7207413cb6b4a13a4ccfdb7a3e2dd2db1
ABC <ABC@DESKTOP-B7B0V14>
2025-12-03 15:17:43 +0530

    First Commit

---
+++ src/test/java/github/GithubPracticeTest.java
@@ -0,0 +1,13 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubPracticeTest {
+@Test
+public void githubPracticeTest() {
+	Reporter.log("github Test",true);
+//	String BROWSER = System.getProperty("browser");
+//	System.out.println(BROWSER);
+}
+}
